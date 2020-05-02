e5cda77ebf1aa41cd58d3e31c1401b3f9a19b69b
Delphine de Sanglier <ddesanglier@gmail.com>
2020-05-02 15:58:23 +1000

    welcome print after names are entered

--- Main_dice.py
+++ Main_dice.py
@@ -2,17 +2,31 @@ import sys
 from dice_throw import dice_throw
 from out_of_prison import out_of_prison
 from free_or_prison import free_or_prison
+from players import players
 
 def main():
 
-    while True:
-        answer = free_or_prison().lower()
-        if answer == 'y':
-            dice_throw()
-        elif answer == 'quit':
-            exit()
-        else :
-            out_of_prison()
+    names = players()
+
+    # Welcome text with all participants type_names
+
+    print('\nNice to meet you', end ="")
+    for i in range(len(names)):
+
+        if i != len(names)-1:
+            print(',', names[i], end ="")
+        else:
+            print(' and', names[len(names)-1], end ="")
+    print(", let's start playing!")
+
+    #while True:
+    #    answer = free_or_prison().lower()
+    #    if answer == 'y':
+    #        dice_throw()
+    #    elif answer == 'quit':
+    #        exit()
+    #    else :
+    #        out_of_prison()
 
 
 # Call to main function to run the program
--- players.py
+++ players.py
@@ -3,9 +3,7 @@ def players():
     names = []
     print("\n*** Welcome to the Dice!***\n")
     type_names = input("\nWrite down each player's name, separated by a comma :\n")
-    players_names = [ x.strip() for x in type_names.split(',')]
-    #players_names.split(',')..strip(' ')
-    #players_names = players_names(map(strip(' '), players_names))
+    players_names = [ x.strip() for x in type_names.split(',') ]
 
     # get the names in an array
     for players_name in players_names:
